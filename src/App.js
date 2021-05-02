60cb3a2cad9d441457785578ac6f09ecc08cf5d9
uttampatro <uttampatro890@gmail.com>
2021-05-02 19:28:49 +0530

    Rearranged

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
+import Rooms from "./Components/Rooms/Rooms";
 import Chat from "./Components/Chats/Chat";
-import Message from "./Components/Messages/Message";
 
 function App() {
   return (
     <div className="app">
       <div className="app_body">
+        <Rooms />
         <Chat />
-        <Message />
       </div>
     </div>
   );
--- src/Components/Chats/Chat.js
+++ src/Components/Chats/Chat.js
@@ -1,47 +1,83 @@
-import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
 import "./Chat.css";
-import { IconButton } from "@material-ui/core";
-import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
-import Room from "../Rooms/Room";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CallOutlinedIcon from "@material-ui/icons/CallOutlined";
+import io from "socket.io-client";
+
+const CONNECTION_PORT = "localhost:5000";
+let socket;
 
 function Chat() {
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT);
+  }, [CONNECTION_PORT]);
+
+
   return (
     <div className="chat">
       <div className="chat_header">
-        <h3>Chats</h3>
+        <Avatar />
+
+        <div className="chat_headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
 
         <div className="chat_headerRight">
           <IconButton>
-            <NotificationsNoneIcon />
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <CallOutlinedIcon />
           </IconButton>
           <IconButton>
             <MoreVertIcon />
           </IconButton>
         </div>
       </div>
-      
-      <div className="chat_search">
-        <div className="chat_searchContainer">
-          <SearchOutlined />
-          <input placeholder="Search User..." type="text" />
-        </div>
+
+      <div className="chat_body">
+
+        <p className="chat_reciever">
+          <span className="chat_name">Patro</span>
+          Hello how r u ?<span className="chat_timestamp">time: 2:00</span>
+        </p>
+        <p className="chat_message">
+          <span className="chat_name">Uttam</span>
+          heya I am fine!
+          <span className="chat_timestamp">time: 2:00</span>
+        </p>
       </div>
 
-      <div className="chat_sidebar">
-      <Room />
-      <Room />
-      <Room />
-      <Room />
-      <Room />
-      <Room />
-      <Room />
-      <Room />
-      <Room />
+      <div className="chat_footer">
+        <AttachFileIcon />
+
+        <form>
+          <input
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            type="text"
+            placeholder="Type a message"
+          />
+          <InsertEmoticonIcon />
+          <button type="submit">
+            Send a meassage
+          </button>
+        </form>
       </div>
     </div>
   );
 }
 
 export default Chat;
+
+{
+  /* <p className={`message_chat ${message.received && "message_reciever"}`}> */
+}
