dcc5b773341b8ffee912bc9147afc174204934cb
uttampatro <uttampatro@rentkars-MacBook-Air.local>
2021-06-02 13:14:48 +0530

    changed

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,7 @@ const isProduction = process.env.REACT_APP_ENVIRONMENT === 'production';
 const isDevelopment = process.env.REACT_APP_ENVIRONMENT === 'development';
 
 const localApiConfig = {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://localhost:5000',
 };
 const prodApiConfig = {
     baseUrl: 'http://localhost:8080', //TODO: put heroku url
